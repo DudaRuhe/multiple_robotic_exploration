1bae0beb5d890edd1329276afe9cd6d2cbbeab92
DudaRuhe <41337947+DudaRuhe@users.noreply.github.com>
2023-08-02 13:51:19 -0300

    Update __init__.py

--- two-agents/two_agents/__init__.py
+++ two-agents/two_agents/__init__.py
@@ -1,6 +1,6 @@
 from gym.envs.registration import register
 
 register(
-    id="twoAgents-v0",
-    entry_point="two_agents.envs:TwoAgentsEnv",
+    id="multioAgents-v0",
+    entry_point="multi_agents.envs:MultiAgentsEnv",
 )
